ab08a0e0a5b6649e6fc71e0f9d3cf3ac23d86cec
noelle <noellemendoza14@gmail.com>
2018-04-09 19:36:48 +0800

    Updated the index.html, style.css, and script.js

--- script.js
+++ script.js
@@ -54,3 +54,99 @@ function show7() {
     g.style.display = "none";
   }
 }
+function showWWW() {
+  var h = document.getElementById("WWW");
+  if (h.style.display === "none") {
+    h.style.display = "block";
+  } else {
+    h.style.display = "none";
+  }
+}
+function showBirth() {
+  var i = document.getElementById("birth");
+  if (i.style.display === "none") {
+    i.style.display = "block";
+  } else {
+    i.style.display = "none";
+  }
+}
+function showInternet() {
+  var j = document.getElementById("internet");
+  if (j.style.display === "none") {
+    j.style.display = "block";
+  } else {
+    j.style.display = "none";
+  }
+}
+function showHttpVer() {
+  var k = document.getElementById("httpVer");
+  if (k.style.display === "none") {
+    k.style.display = "block";
+  } else {
+    k.style.display = "none";
+  }
+}
+function showHttp() {
+  var l = document.getElementById("HTTP");
+  if (l.style.display === "none") {
+    l.style.display = "block";
+  } else {
+    l.style.display = "none";
+  }
+}
+function showResourceAdd() {
+  var m = document.getElementById("resourceAdd");
+  if (m.style.display === "none") {
+    m.style.display = "block";
+  } else {
+    m.style.display = "none";
+  }
+}
+function showHttpMessage() {
+  var n = document.getElementById("httpMessage");
+  if (n.style.display === "none") {
+    n.style.display = "block";
+  } else {
+    n.style.display = "none";
+  }
+}
+function showMesStruc() {
+  var o = document.getElementById("mesStruc");
+  if (o.style.display === "none") {
+    o.style.display = "block";
+  } else {
+    o.style.display = "none";
+  }
+}
+function showHTML() {
+  var p = document.getElementById("html");
+  if (p.style.display === "none") {
+    p.style.display = "block";
+  } else {
+    p.style.display = "none";
+  }
+}
+function showWHATWG() {
+  var q = document.getElementById("whatwg");
+  if (q.style.display === "none") {
+    q.style.display = "block";
+  } else {
+    q.style.display = "none";
+  }
+}
+function showXHTML() {
+  var r = document.getElementById("xhtml");
+  if (r.style.display === "none") {
+    r.style.display = "block";
+  } else {
+    r.style.display = "none";
+  }
+}
+function showHeaders() {
+  var s = document.getElementById("headers");
+  if (s.style.display === "none") {
+    s.style.display = "block";
+  } else {
+    s.style.display = "none";
+  }
+}
